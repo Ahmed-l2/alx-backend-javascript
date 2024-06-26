a3e589b45f0d70c7cab21157bfda59f61581752a
Ahmed <ahmed_l2@yahoo.com>
2024-06-26 23:39:33 +0300

    6. Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,17 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise.all([signUpUser(firstName, lastName), uploadPhoto(filename)])
-    .then((values) => values);
+    .then((values) => {
+      const response = [];
+
+      for (const element of values) {
+        if (element.status === 'fulfilled') {
+          response.push({ status: element.status, value: element.value });
+        } else {
+          response.push({ status: element.status, value: `${element.reason}` });
+        }
+      }
+
+      return response;
+    });
 }
