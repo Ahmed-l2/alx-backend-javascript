38cfbd364bf908a58778273b90286fb8a82c4a5a
Ahmed <ahmed_l2@yahoo.com>
2024-07-03 01:17:04 +0300

    8. Clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+export default function cleanSet(set, startString) {
+  if (!startString) {
+    return '';
+  }
+  const result = [...set]
+    // First we filter elements starting with startString
+    .filter((value) => value.startsWith(startString))
+    // Then we map to substring after startString
+    .map((value) => value.substring(startString.length))
+    // And finally we join elements with '-'
+    .join('-');
+
+  return result;
+}
