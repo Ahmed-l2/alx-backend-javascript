ba56c27b8cad8f59a9f6818fbfd8bdd2f95f1ed2
Ahmed <ahmed_l2@yahoo.com>
2024-08-19 22:31:13 +0300

    1. Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
-console.log('Welcome to Holberton School, what is your name?');
-
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
-  process.exit();
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
-
 process.on('exit', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
