63c87766c21b010d4feaafe288a5140e91f2e64f
Ahmed <ahmed_l2@yahoo.com>
2024-08-20 01:13:05 +0300

    3. Reading a file asynchronously with Node JS

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,32 +1,35 @@
 #!/usr/bin/node
 const fs = require('fs');
+const { resolve } = require('path');
 
-async function countStudents(path) {
-  try {
-    const data = await fs.readFile(path, 'utf8');
-    const lines = data.split('\n').filter(line => line.trim() !== '').slice(1);
-
-    console.log(`Number of students: ${lines.length}`);
-
-    const cs = [];
-    const swe = [];
-
-    lines.forEach((line) => {
-      if (line.includes('CS')) {
-        const student = line.split(',')[0];
-        cs.push(student);
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        throw new Error('Cannot load the database');
       }
-      if (line.includes('SWE')) {
-        const student = line.split(',')[0];
-        swe.push(student);
-      }
+      const lines = data.split('\n').slice(1, -1);
+      console.log(`Number of students: ${lines.length}`);
+
+      const cs = [];
+      const swe = [];
+
+      lines.forEach((line) => {
+        if (line.includes('CS')) {
+          const student = line.split(',')[0];
+          cs.push(student);
+        }
+        if (line.includes('SWE')) {
+          const student = line.split(',')[0];
+          swe.push(student);
+        }
+      });
+
+      console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
+      console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
+      return resolve();
     });
-
-    console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
-    console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
-  } catch (err) {
-    throw new Error('Cannot load the database');
-  }
+  });
 }
 
 module.exports = countStudents;
