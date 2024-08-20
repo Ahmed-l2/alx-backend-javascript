beb8db00c04b66d59edd8e87b2e1e67c891bea5e
Ahmed <ahmed_l2@yahoo.com>
2024-08-21 00:19:29 +0300

    5. Create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -11,13 +11,8 @@ const app = http.createServer(async (req, res) => {
     res.statusCode = 200;
     res.write('This is the list of our students\n');
     try {
-      if (process.argv[2]) {
-        const data = await countStudents(process.argv[2]);
-        res.end(data);
-      } else {
-        const studentsData = await countStudents('database.csv');
-        res.end(studentsData);
-      }
+      const studentsData = await countStudents('database.csv');
+      res.end(studentsData);
     } catch (err) {
       res.statusCode = 500;
       res.end('Error reading data');
