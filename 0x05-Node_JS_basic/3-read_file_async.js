349868a66e21006b10a2dc0b20e0290a826324bc
Ahmed <ahmed_l2@yahoo.com>
2024-08-20 22:00:10 +0300

    3. Reading a file asynchronously with Node JS

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -2,35 +2,35 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  const promise = new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (err, data) => {
       if (err) {
-        reject(Error('Cannot load the database'));
+        reject(new Error('Cannot load the database'));
+        return;
       }
 
-      const lines = data.split('\n').slice(1, -1);
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      const students = lines.slice(1);
 
       const cs = [];
       const swe = [];
 
-      lines.forEach((line) => {
-        if (line.includes('CS')) {
-          const student = line.split(',')[0];
-          cs.push(student);
-        }
-        if (line.includes('SWE')) {
-          const student = line.split(',')[0];
-          swe.push(student);
+      students.forEach((line) => {
+        const [name, , , field] = line.split(',');
+        if (field === 'CS') {
+          cs.push(name);
+        } else if (field === 'SWE') {
+          swe.push(name);
         }
       });
 
-      console.log(`Number of students: ${lines.length}`);
+      console.log(`Number of students: ${students.length}`);
       console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
       console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
-      resolve(`Number of students: ${lines.length}\nNumber of students in CS: ${cs.length}. List: ${cs.join(', ')}\nNumber of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
+
+      resolve(`Number of students: ${students.length}\nNumber of students in CS: ${cs.length}. List: ${cs.join(', ')}\nNumber of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
     });
   });
-  return promise;
 }
 
 module.exports = countStudents;
