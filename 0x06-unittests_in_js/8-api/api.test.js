74a4b1071c0a737468226a57e120690170400937
Ahmed <ahmed_l2@yahoo.com>
2024-08-23 00:29:49 +0300

    9. Regex integration testing

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -3,7 +3,7 @@ const chai = require('chai');
 const expect = chai.expect;
 
 describe('API', () => {
-  it('should return the correct response for GET /', (done) => {
+  it('Returns the correct response for GET /', (done) => {
     const options = {
       url: 'http://localhost:7865',
       method: 'GET',
@@ -18,7 +18,7 @@ describe('API', () => {
     });
   });
 
-  it('should return 404 for non-existent routes', (done) => {
+  it('Returns 404 for non-existent routes', (done) => {
     const options = {
       url: 'http://localhost:7865/nonexistent',
       method: 'GET',
@@ -26,6 +26,34 @@ describe('API', () => {
 
     request(options, (error, response, body) => {
       expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+
+  it('Returns the correct response for GET /cart/:id', (done) => {
+    const options = {
+      url: 'http://localhost:7865/cart/123',
+      method: 'GET',
+    };
+
+    request(options, (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 123');
+      expect(response.headers['content-type']).to.include('text/html');
+      done();
+    });
+  });
+
+  it('Returns 404 response for incorrect CartID for GET /cart/:id', (done) => {
+    const options = {
+      url: 'http://localhost:7865/cart/asd',
+      method: 'GET',
+    };
+
+    request(options, (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      expect(body).to.equal('Invalid cart ID. Must be a positive integer.');
+      expect(response.headers['content-type']).to.include('text/html');
       done();
     });
   });
